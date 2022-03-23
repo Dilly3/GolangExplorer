3c6c7abd6a48704d205cbe02a64c4529ac0c77ac
Anikammadu Michael <dil.anikamadu@gmail.com>
2022-03-23 01:34:04 +0100

    appendDelete Codewars

--- Algo/Week5/appenddelete.go
+++ Algo/Week5/appenddelete.go
@@ -1,1 +1,25 @@
 package week5
+
+//function isItPossibleToModify($s, $t, $k) {
+//$sLength = strlen($s);
+//$tLength = strlen($t);
+//
+//if ($sLength < $tLength) {
+//return ($tLength - $sLength) % 2 == 0;
+//}
+//
+//$commonPrefix = 0;
+//
+//// find a largest common prefix between s and t
+//for ($i = 0; $i < min($sLength, $tLength); $i++) {
+//if ($s[$i] != $t[$i]) {
+//break;
+//}
+//
+//$commonPrefix++;
+//}
+//
+//$needToProcess = ($tLength - $commonPrefix) + ($sLength - $commonPrefix);
+//
+//return $needToProcess <= $k;
+//}
