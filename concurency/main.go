3fc21374fb3917099a28512a53dfe127ed3d2a70
dilly3 <dil.anikamadu@gmail.com>
2022-07-06 00:52:11 +0100

    concurency v1

---
+++ concurency/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"time"
+)
+
+func main() {
+	go echo(os.Stdin, os.Stdout)
+	time.Sleep(30 * time.Second)
+	fmt.Println("timed out")
+	os.Exit(0)
+
+}
+func echo(in io.Reader, out io.Writer) {
+
+	io.Copy(out, in)
+}
